d01666b35c225a8dc500866e212167d5e52d9f7e
Fabricio Miranda <fbrnery@gmail.com>
2022-08-23 19:22:49 -0300

    Adcionei validações no model article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
     has_one_attached :featured_image
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+    validates :author, presence: true
+
 end
